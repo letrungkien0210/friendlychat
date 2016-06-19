3697b3c702378bcbcea44f117f2b339d519cab63
letrungkien0210 <letrungkien0210@gmail.com>
2016-06-19 17:34:07 +0700

    Fix error when Avatar is empty

--- android/app/src/main/java/com/google/firebase/codelab/friendlychat/mvvm/ui/base/BindingAdapter.java
+++ android/app/src/main/java/com/google/firebase/codelab/friendlychat/mvvm/ui/base/BindingAdapter.java
@@ -1,6 +1,10 @@
 package com.google.firebase.codelab.friendlychat.mvvm.ui.base;
 
+import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
+
 import com.bumptech.glide.Glide;
+import com.google.firebase.codelab.friendlychat.R;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -12,8 +16,13 @@ public class BindingAdapter {
 
     @android.databinding.BindingAdapter("app:avatar")
     public static void setAvatar(CircleImageView view, String avatar) {
-        Glide.with(view.getContext())
-                .load(avatar)
-                .into(view);
+
+        if(TextUtils.isEmpty(avatar)){
+            view.setImageDrawable(ContextCompat.getDrawable(view.getContext(), R.drawable.ic_account_circle_black_36dp));
+        }else{
+            Glide.with(view.getContext())
+                    .load(avatar)
+                    .into(view);
+        }
     }
 }
